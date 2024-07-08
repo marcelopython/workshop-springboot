87b350ebe6fc6952f3d01aaa2c0f545b1ff0b2d2
Marcelo <msr88957@gmail.com>
2024-07-08 13:16:05 -0300

    get total

--- src/main/java/com/estudo/curso/entities/Order.java
+++ src/main/java/com/estudo/curso/entities/Order.java
@@ -43,6 +43,7 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 	
@@ -101,6 +102,14 @@ public class Order implements Serializable {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
